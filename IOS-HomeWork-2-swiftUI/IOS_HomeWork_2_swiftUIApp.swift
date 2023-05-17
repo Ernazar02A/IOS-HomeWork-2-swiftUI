87365ba056f50f7640c742b97a3933d0a453fa1d
Ernazar02A <119403073+Ernazar02A@users.noreply.github.com>
2023-05-17 08:35:50 +0600

    Initial Commit

---
+++ IOS-HomeWork-2-swiftUI/IOS_HomeWork_2_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_HomeWork_2_swiftUIApp.swift
+//  IOS-HomeWork-2-swiftUI
+//
+//  Created by MacBook Pro on 17/5/23.
+//
+
+import SwiftUI
+
+@main
+struct IOS_HomeWork_2_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
